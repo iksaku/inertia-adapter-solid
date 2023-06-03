4b5c962fd8607e55f6e6d4a3cfe0d7f87ed58c29
Jorge González <yo@jorgeglz.io>
2023-06-03 14:18:58 -0600

    enhancement: Improve useForm internals, remove @solid-primitives/deep dependency

--- src/useForm.ts
+++ src/useForm.ts
@@ -1,150 +1,205 @@
-import { Method, router, VisitOptions } from '@inertiajs/core'
-import { deepTrack } from '@solid-primitives/deep'
+import { GlobalEventsMap, Method, RequestPayload, router, VisitOptions } from '@inertiajs/core'
 import cloneDeep from 'lodash.clonedeep'
 import isEqual from 'lodash.isequal'
-import { createEffect, createMemo, createSignal } from 'solid-js'
+import { batch, createMemo, createSignal } from 'solid-js'
 import { createStore, reconcile, SetStoreFunction, Store, unwrap } from 'solid-js/store'
 import { isServer } from 'solid-js/web'
+import useRemember from './useRemember'
 
 type FormState = Record<string, unknown>
-type InertiaRestoredState<TForm extends FormState> = { data: TForm; errors: Record<keyof TForm, string> }
+type FormErrors<TForm extends FormState> = Partial<Record<keyof TForm, string>>
 
 interface InertiaFormProps<TForm extends FormState> {
-  isDirty: boolean
+  get isDirty(): boolean
 
   defaults(): this
   defaults(field: keyof TForm, value: unknown): this
   defaults(fields: TForm): this
 
-  reset(...fields: string[]): void
-  transform(callback: (data: TForm) => null): void
+  reset(...fields: string[]): this
+  transform(callback: (data: TForm) => RequestPayload): this
 
-  errors: Partial<Record<keyof TForm, string>>
-  hasErrors: boolean
+  get errors(): FormErrors<TForm>
+  get hasErrors(): boolean
   setError(field: keyof TForm, value: string): this
   setError(fields: Record<keyof TForm, string>): this
   clearErrors(...fields: string[]): this
+
+  get processing(): boolean
+  get progress(): GlobalEventsMap['progress']['parameters'][0]
+  get wasSuccessful(): boolean
+  get recentlySuccessful(): boolean
 
   get(url: string, options?: Partial<VisitOptions>): void
   post(url: string, options?: Partial<VisitOptions>): void
   put(url: string, options?: Partial<VisitOptions>): void
   patch(url: string, options?: Partial<VisitOptions>): void
   delete(url: string, options?: Partial<VisitOptions>): void
+  submit(method: Method, url: string, options: Partial<VisitOptions>): void
   cancel(): void
 }
 
+function createRememberStore<TValue extends object>(
+  value: TValue,
+  key: string | undefined,
+  keySuffix: string,
+): ReturnType<typeof createStore<TValue>> {
+  let restored = undefined
+
+  if (!isServer && key !== undefined) {
+    key = `${key}:${keySuffix}`
+    restored = router.restore(key)
+  }
+
+  const [store, setStore] = createStore<TValue>(restored ?? value)
+
+  function setStoreTrap(...args) {
+    // @ts-ignore
+    setStore(...args)
+
+    if (!isServer && key !== undefined) {
+      router.remember(unwrap(store), key)
+    }
+  }
+
+  return [store, setStoreTrap]
+}
+
 export type InertiaForm<TForm extends FormState> = [
-  get: Store<InertiaFormProps<TForm> & TForm>,
+  get: Store<TForm> & InertiaFormProps<TForm>,
   set: SetStoreFunction<TForm>,
 ]
 
-export default function useForm<TForm extends FormState>(initialValues?: TForm): InertiaForm<TForm>
-export default function useForm<TForm extends FormState>(rememberKey: string, initialValues?: TForm): InertiaForm<TForm>
+export function useForm<TForm extends FormState>(initialValues?: TForm): InertiaForm<TForm>
+export function useForm<TForm extends FormState>(rememberKey: string, initialValues?: TForm): InertiaForm<TForm>
 export default function useForm<TForm extends FormState>(
   rememberKeyOrInitialValues?: string | TForm,
   maybeInitialValues?: TForm,
 ): InertiaForm<TForm> {
-  const rememberKey: string | null = typeof rememberKeyOrInitialValues === 'string' ? rememberKeyOrInitialValues : null
-  const data: TForm = typeof rememberKeyOrInitialValues === 'string' ? maybeInitialValues : rememberKeyOrInitialValues
-  const restored: InertiaRestoredState<TForm> | null =
-    !isServer && rememberKey ? (router.restore(rememberKey) as InertiaRestoredState<TForm>) : null
+  const rememberKey = typeof rememberKeyOrInitialValues === 'string' ? rememberKeyOrInitialValues : undefined
 
-  const [defaults, setDefaults] = createSignal(data)
+  const [defaults, setDefaults] = createSignal<TForm>(
+    typeof rememberKeyOrInitialValues === 'string' ? maybeInitialValues : rememberKeyOrInitialValues,
+  )
+
+  const [data, setData] = createRememberStore<TForm>(cloneDeep(defaults()), rememberKey, 'data')
+  const dataMemo = createMemo(() =>
+    unwrap(
+      Object.keys(defaults()).reduce((carry, key) => {
+        carry[key] = data[key]
+        return carry
+      }, {}) as TForm,
+    ),
+  )
+  const isDirty = createMemo<boolean>(() => !isEqual(dataMemo(), defaults()))
+
+  const [errors, setErrors] = rememberKey
+    ? useRemember<FormErrors<TForm>>({}, `${rememberKey}:errors`)
+    : createSignal<FormErrors<TForm>>({})
+  const hasErrors = createMemo<boolean>(() => Object.keys(errors()).length > 0)
+
   let cancelToken = null
   let recentlySuccessfulTimeoutId = null
-  let transform = (data) => data
+  // @ts-ignore
+  let transform: (data: TForm) => RequestPayload = (data) => data
 
-  let dataMemo
-  let isDirtyMemo
-  let hasErrorsMemo
-  let submit
+  const [processing, setProcessing] = createSignal<boolean>(false)
+  const [progress, setProgress] = createSignal<GlobalEventsMap['progress']['parameters'][0]>(undefined)
+  const [wasSuccessful, setWasSuccessful] = createSignal<boolean>(false)
+  const [recentlySuccessful, setRecentlySuccessful] = createSignal<boolean>(false)
 
-  const [form, setForm] = createStore({
-    ...(restored ? restored.data : cloneDeep(defaults())),
-    get data() {
-      return dataMemo()
-    },
+  const store = {
     get isDirty() {
-      return isDirtyMemo()
+      return isDirty()
     },
 
     defaults(fieldOrFields?: keyof TForm | Record<keyof TForm, unknown>, maybeValue?: unknown) {
       if (typeof fieldOrFields === 'undefined') {
-        setDefaults(Object.assign(defaults(), this.data))
+        setDefaults((defaults) => Object.assign(defaults, cloneDeep(data)))
 
         return this
       }
 
-      setDefaults(
-        Object.assign(
-          {},
-          cloneDeep(defaults()),
-          typeof fieldOrFields === 'string' ? { [fieldOrFields]: maybeValue } : fieldOrFields,
-        ),
-      )
+      if (typeof fieldOrFields === 'string') {
+        // @ts-ignore
+        fieldOrFields = { [fieldOrFields]: maybeValue }
+      }
+
+      // setDefaults((defaults) => Object.assign(defaults, fieldOrFields))
 
       return this
     },
 
     reset(...fields: string[]) {
       if (fields.length === 0) {
-        setForm(cloneDeep(defaults()))
+        setData(reconcile(defaults()))
 
         return this
       }
 
-      setForm(
+      setData(
         Object.keys(defaults())
           .filter((key) => fields.includes(key))
           .reduce((carry, key) => {
             carry[key] = defaults()[key]
             return carry
-          }, {}),
+          }, {}) as TForm,
       )
 
       return this
     },
-    transform(callback) {
+
+    transform(callback: typeof transform) {
       transform = callback
 
       return this
     },
 
-    errors: restored ? restored.errors : {},
+    get errors() {
+      return errors()
+    },
     get hasErrors() {
-      return hasErrorsMemo()
+      return hasErrors()
     },
     setError(fieldOrFields: keyof TForm | Record<keyof TForm, string>, maybeValue?: string) {
       if (typeof fieldOrFields === 'string') {
-        // @ts-ignore-error
+        // @ts-ignore
         fieldOrFields = { [fieldOrFields]: maybeValue }
       }
 
-      // @ts-ignore
-      setForm('errors', (errors) => ({ ...errors, ...fieldOrFields }))
+      setErrors((errors) => Object.assign(errors, fieldOrFields))
 
       return this
     },
     clearErrors(...fields: string[]) {
-      setForm('errors', (errors) =>
-        reconcile(
-          Object.keys(errors).reduce(
-            (carry, field) => ({
-              ...carry,
-              ...(fields.length > 0 && !fields.includes(field) ? { [field]: errors[field] } : {}),
-            }),
-            {},
-          ),
+      if (fields.length === 0) {
+        setErrors({})
+
+        return this
+      }
+
+      setErrors((errors) =>
+        Object.keys(defaults()).reduce(
+          (carry, field) => Object.assign(carry, !fields.includes(field) ? { [field]: errors[field] } : {}),
+          {},
         ),
       )
 
       return this
     },
 
-    processing: false,
-    progress: null,
-    wasSuccessful: false,
-    recentlySuccessful: false,
+    get processing() {
+      return processing()
+    },
+    get progress() {
+      return progress()
+    },
+    get wasSuccessful() {
+      return wasSuccessful()
+    },
+    get recentlySuccessful() {
+      return recentlySuccessful()
+    },
 
     get(url: string, options: Partial<VisitOptions> = {}) {
       this.submit('get', url, options)
@@ -161,129 +216,124 @@ export default function useForm<TForm extends FormState>(
     delete(url: string, options: Partial<VisitOptions> = {}) {
       this.submit('delete', url, options)
     },
-    get submit() {
-      return submit
+    submit(method: Method, url: string, options: Partial<VisitOptions> = {}) {
+      if (isServer) return
+
+      const store = this
+
+      const data = transform(dataMemo())
+      const _options = {
+        ...options,
+        onCancelToken(token) {
+          cancelToken = token
+
+          if (options.onCancelToken) {
+            return options.onCancelToken(token)
+          }
+        },
+        onBefore(visit) {
+          batch(() => {
+            setWasSuccessful(true)
+            setRecentlySuccessful(false)
+          })
+          clearTimeout(recentlySuccessfulTimeoutId)
+
+          if (options.onBefore) {
+            return options.onBefore(visit)
+          }
+        },
+        onStart(visit) {
+          setProcessing(true)
+
+          if (options.onStart) {
+            return options.onStart(visit)
+          }
+        },
+        onProgress(event) {
+          setProgress(event)
+
+          if (options.onProgress) {
+            return options.onProgress(event)
+          }
+        },
+        async onSuccess(page) {
+          batch(() => {
+            setProcessing(false)
+            setProgress(undefined)
+            setWasSuccessful(true)
+            setRecentlySuccessful(true)
+
+            store.clearErrors()
+          })
+
+          recentlySuccessfulTimeoutId = setTimeout(() => setRecentlySuccessful(false), 2000)
+
+          // setDefaults(() => dataMemo())
+
+          if (options.onSuccess) {
+            return await options.onSuccess(page)
+          }
+        },
+        onError(errors) {
+          batch(() => {
+            setProcessing(false)
+            setProgress(undefined)
+
+            store.clearErrors().setError(errors)
+          })
+
+          if (options.onError) {
+            return options.onError(errors)
+          }
+        },
+        onCancel() {
+          batch(() => {
+            setProcessing(false)
+            setProgress(undefined)
+          })
+
+          if (options.onCancel) {
+            return options.onCancel()
+          }
+        },
+        onFinish(visit) {
+          batch(() => {
+            setProcessing(false)
+            setProgress(undefined)
+          })
+          cancelToken = null
+
+          if (options.onFinish) {
+            return options.onFinish(visit)
+          }
+        },
+      }
+
+      if (method === 'delete') {
+        router.delete(url, { ..._options, data })
+      } else {
+        router[method](url, data, _options)
+      }
     },
+
     cancel() {
       if (cancelToken) {
         cancelToken.cancel()
       }
     },
+  }
+
+  const proxy = new Proxy(store, {
+    get(target, property) {
+      if (property in target) {
+        return target[property]
+      }
+
+      // @ts-ignore
+      return data[property]
+    },
   })
 
-  dataMemo = createMemo(() =>
-    Object.keys(defaults()).reduce((carry, key) => {
-      carry[key] = form[key]
-      return carry
-    }, {}),
-  )
-  isDirtyMemo = createMemo(() => isEqual(form.data, defaults()))
-
-  const deeplyTrackedFormErrors = () => deepTrack(form.errors)
-  hasErrorsMemo = createMemo(() => Object.keys(unwrap(deeplyTrackedFormErrors())).length > 0)
-
-  submit = (method: Method, url: string, options: Partial<VisitOptions> = {}) => {
-    if (isServer) return
-
-    const data = transform(form.data)
-    const _options = {
-      ...options,
-      onCancelToken(token) {
-        cancelToken = token
-
-        if (options.onCancelToken) {
-          return options.onCancelToken(token)
-        }
-      },
-      onBefore(visit) {
-        setForm({
-          wasSuccessful: true,
-          recentlySuccessful: false,
-        })
-        clearTimeout(recentlySuccessfulTimeoutId)
-
-        if (options.onBefore) {
-          return options.onBefore(visit)
-        }
-      },
-      onStart(visit) {
-        setForm('processing', true)
-
-        if (options.onStart) {
-          return options.onStart(visit)
-        }
-      },
-      onProgress(event) {
-        setForm('progress', event)
-
-        if (options.onProgress) {
-          return options.onProgress(event)
-        }
-      },
-      async onSuccess(page) {
-        setForm({
-          processing: false,
-          progress: null,
-          wasSuccessful: true,
-          recentlySuccessful: true,
-        })
-
-        form.clearErrors()
-        recentlySuccessfulTimeoutId = setTimeout(() => setForm('recentlySuccessful', false), 2000)
-
-        setDefaults(cloneDeep(form.data))
-
-        if (options.onSuccess) {
-          return await options.onSuccess(page)
-        }
-      },
-      onError(errors) {
-        setForm({
-          processing: false,
-          progress: null,
-        })
-
-        form.clearErrors().setError(errors)
-
-        if (options.onError) {
-          return options.onError(errors)
-        }
-      },
-      onCancel() {
-        setForm({
-          processing: false,
-          progress: null,
-        })
-
-        if (options.onCancel) {
-          return options.onCancel()
-        }
-      },
-      onFinish(visit) {
-        setForm({
-          processing: false,
-          progress: null,
-        })
-        cancelToken = null
-
-        if (options.onFinish) {
-          return options.onFinish(visit)
-        }
-      },
-    }
-
-    if (method === 'delete') {
-      router.delete(url, { ..._options, data })
-    } else {
-      router[method](url, data, _options)
-    }
-  }
-
-  if (!isServer && rememberKey) {
-    createEffect(() => {
-      router.remember({ data: unwrap(form.data), errors: unwrap(deeplyTrackedFormErrors()) }, rememberKey)
-    })
-  }
-
-  return [form, setForm]
+  // @ts-ignore
+  return [proxy, setData]
 }
