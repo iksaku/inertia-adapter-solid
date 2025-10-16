4b4b0b895b63515612ffca02e7fd5f23e4e8f4d5
Jorge González <yo@jorgeglz.io>
2025-10-16 15:10:01 -0600

    fix(exports): Export `usePoll()` utility

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ export { default as Deferred } from './Deferred'
 export { default as Link } from './Link'
 export { default as useForm } from './useForm'
 export { default as usePage } from './usePage'
-// TODO: usePoll
+export { default as usePoll } from './usePoll'
 // TODO: usePrefetch
 export { default as useRemember } from './useRemember'
 export { default as WhenVisible } from './WhenVisible'
